d75369c6174e1fb5850074e9afc96831bfd32c65
daryasv <svirskydarya@gmail.com>
2021-11-26 21:10:02 +0200

    changes

--- Project1/DataStructureProgram.cpp
+++ Project1/DataStructureProgram.cpp
@@ -1,54 +1,82 @@
-#include <stdio.h>
+#include <windows.h>
+#include <process.h>
+#include <iostream>
+#include <sstream>
+#include <conio.h>
 #include <stdlib.h>
+#include <sstream> 
+#include <iostream>
+#include <vector>
+using namespace std;
 
 
 #define _CRT_SECURE_NO_WARNINGS
 
 
 void findSumInrecursiveWay(int* A, int n, int x, int y) {
-	if ((n == 1) && (y!=0)) {
-		if (x = A[0] + y) {
-			printf("%d %d", y, A[0]);
+	if ((n == 1) && (y != 0)) {
+		if (x == A[0] + y) {
+			cout << y << " " << A[0] << "\n";
 		}
-		else if ((n == 1) && (y == 0)) {
-			printf("");
+	}
+	if ((n == 1) && (y == 0)) {
+		cout << "";
+	}
+
+	else  if (n != 1) {
+		if (y != 0) {
+			if (x == A[n - 1] + y) {
+				cout << y << " " << A[n - 1] << "\n";
+			}
+			findSumInrecursiveWay(A, n - 1, x, y);
 		}
-		
-		else if (n != 1) {
-			if (y!=0)
-			{
-				if (x = A[n - 1] + y) {
-					printf("%d %d", y, A[n - 1]);
+		else if (y == 0) {
+			findSumInrecursiveWay(A, n - 1, x, A[n - 1]);
+			findSumInrecursiveWay(A, n - 1, x, 0);
+		}
+	}
+}
+
+
+bool isPrinted(vector <vector <int>> printedIndexes, int i, int j) {
+	for (int z = 0; z < printedIndexes.size(); z++) {
+		if (((printedIndexes[z][0] == i && printedIndexes[z][1] == j)) || ((printedIndexes[z][1] == i && printedIndexes[z][0] == j))) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+void findSumInIterativeWay(int* nativeNumbers, int n, int x) {
+	vector <vector <int>> printedIndexes;
+	int iVec = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if ((i != j) && (x == nativeNumbers[i] + nativeNumbers[j])) {
+				if (!isPrinted(printedIndexes, i, j)) {
+					cout << nativeNumbers[i] << " " << nativeNumbers[j] << "\n";
+					vector<int> v;
+					v.push_back(i);
+					v.push_back(j);
+					printedIndexes.push_back(v);
+
 				}
-				findSumInrecursiveWay(A, n - 1, x, y);
 			}
-			else {
-				findSumInrecursiveWay(A, n-1, x, A[n - 1]);
-				findSumInrecursiveWay(A, n-1,x, 0);
-			}
+		}
 	}
-	}
-
 }
 
-
-void findSumInIterativeWay(int * nativeNumbers, int n, int x) {
-	for (int i = 0; i < n-1; i++) {
-		if (nativeNumbers[i] + nativeNumbers[i + 1] == x)
-			printf("%d %d", nativeNumbers[i], nativeNumbers[i + 1]);
-	}
-}
 
 int main() {
 	int n;
-	//scanf("%d", &n);
-
-
 
 	//test
-	int arr[5] = { 1,2,3,4,5};
-	int size = 5;
+	int arr[8] = { 5,5,4,5,2,4,3,1 };
+	int size = 8;
 	int x = 3;
 	findSumInIterativeWay(arr, size, x);
-	
+	//findSumInrecursiveWay(arr, size, x,0);
+
 }
